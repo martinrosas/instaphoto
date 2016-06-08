5565aacab003d3ad5d06b5a07312ede4c2ead60e
martin rosas <martinleonrosas@gmail.com>
2016-06-07 19:11:56 -0700

    show and index page

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,20 +1,34 @@
 class PicsController < ApplicationController
-
+  before_action :find_Pic, only: [:show, :edit, :update, :destroy]
     def index
+    end
+
+    def show
+      # to show you a pic we must first find that pic
     end
 
     def new
       @pic = Pic.new
     end
-
+    # once pic is created allows next steps
     def create
       @pic = Pic.new(pic_params)
+
+      if @pic.save
+        redirect_to @pic, notice: "Yes! It was posted!"
+      else
+        render 'new'
+      end
     end
 
     private
 
     def pic_params
       params.require(:pic).permit(:title, :description)
+    end
+
+    def find_Pic
+      @pic = Pic.find(params[:id])
     end
 
 end
