a9a672283f10b31ccf2d1c986fbf0eab02049508
martin rosas <martinleonrosas@gmail.com>
2016-06-25 05:01:42 -0700

    paperclip gem and pictures

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,3 +1,8 @@
 class Pic < ActiveRecord::Base
   belongs_to :user
+
+# makes pic upload possible.
+  has_attached_file :image, :styles => { :medium => "300x300>" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
+
